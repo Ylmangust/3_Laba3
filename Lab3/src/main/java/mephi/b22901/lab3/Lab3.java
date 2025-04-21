c2841b6a74b120520df192114c4587e86ef21ef3
Ylmangust <adeli-2019@mail.ru>
2025-04-21 14:57:33 +0300

    start
    создан репозиторий, прописан код в pom-файле для создания fatJAR

---
+++ Lab3/src/main/java/mephi/b22901/lab3/Lab3.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package mephi.b22901.lab3;
+
+/**
+ *
+ * @author Регина
+ */
+public class Lab3 {
+
+    public static void main(String[] args) {
+    }
+}
